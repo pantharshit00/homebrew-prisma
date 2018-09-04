48bbf69ddae8c0538c924be396aad69624a03b97
Divyendu Singh <divyendu.z@gmail.com>
2018-09-04 13:07:31 +0200

    fix(url): use prisma s3

--- prisma.rb
+++ prisma.rb
@@ -1,7 +1,7 @@
 class Prisma < Formula
   desc "Prisma turns your database into a realtime GraphQL API"
   homepage "https://github.com/prisma/prisma"
-  url "https://s3-ap-southeast-1.amazonaws.com/homebrew-prisma-dev/prisma%401.17.0-alpha.2/prisma-1.17.0-alpha.2.tar.gz"
+  url "https://s3-eu-west-1.amazonaws.com/homebrew-prisma/prisma-1.17.0-alpha.2.tar.gz"
   sha256 "1b9473bce3ae109bfdcab0c6c494c4cae9d7e7c7410f792464b1922a981e9ba3"
   version "1.17.0-alpha.2"
 
