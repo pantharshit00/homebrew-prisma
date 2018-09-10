fdff198fe2cc31a6e103654ed5506e3f231e9e3f
Divyendu Singh <divyendu.z@gmail.com>
2018-09-10 20:26:55 +0200

    bump version to prisma-1.17.0-alpha.41

--- prisma.rb
+++ prisma.rb
@@ -1,9 +1,9 @@
 class Prisma < Formula
   desc "Prisma turns your database into a realtime GraphQL API"
   homepage "https://github.com/prisma/prisma"
-  url "https://s3-eu-west-1.amazonaws.com/homebrew-prisma/prisma-1.17.0-alpha.40.tar.gz"
-  sha256 "f7d5cf2b337d336f4f96456be2cbdfe617548f475e801933df154f98f623a508"
-  version "1.17.0-alpha.40"
+  url "https://s3-eu-west-1.amazonaws.com/homebrew-prisma/prisma-1.17.0-alpha.41.tar.gz"
+  sha256 "62f2a9a95bd40d12d22449166dbb2d25832dd4cc68e14d1f76b9074403e12d24"
+  version "1.17.0-alpha.41"
 
   bottle :unneeded
 
