2ba3874639aa12b9d5df81cfcd4dcfb6dcee6c5b
Divyendu Singh <divyendu.z@gmail.com>
2018-09-11 20:21:55 +0200

    bump hash

--- prisma.rb
+++ prisma.rb
@@ -2,7 +2,7 @@ class Prisma < Formula
   desc "Prisma turns your database into a realtime GraphQL API"
   homepage "https://github.com/prisma/prisma"
   url "https://s3-eu-west-1.amazonaws.com/homebrew-prisma/prisma-1.17.0-beta.tar.gz"
-  sha256 "c4d726c0ffdbaa6e42f8ab368fd18c2197d4cdd31b2ecc13098b648ed57b2fa5"
+  sha256 "073a0d4c39ba267f1bcda798816b91092fa154806f8d20556b98e3b054f5764a"
   version "1.17.0-beta"
 
   bottle :unneeded
