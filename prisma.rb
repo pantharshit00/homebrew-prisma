92d398729e75cb085d7f9bd06d6f28aed286c964
Divyendu Singh <divyendu.z@gmail.com>
2018-10-24 19:12:52 +0530

    bump version to 1.19.0 - fix checksum

--- prisma.rb
+++ prisma.rb
@@ -2,7 +2,7 @@ class Prisma < Formula
   desc "Prisma turns your database into a realtime GraphQL API"
   homepage "https://github.com/prisma/prisma"
   url "https://s3-eu-west-1.amazonaws.com/homebrew-prisma/prisma-1.19.0.tar.gz"
-  sha256 "2517026215d5410cec07c0fe82ac11d60c2c201503f238ea16c7b48d7b7dcd84"
+  sha256 "bed08fbbd0ef7763046c17c61d5987f42e7f148d6d32b6f33fca70e9be7fd9ec"
   version "1.19.0"
 
   bottle :unneeded
