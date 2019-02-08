9e285baf86207394b58c5b119e143e9abecacd7e
Harshit Pant <pantharshit00@gmail.com>
2019-02-09 00:53:46 +0530

    bump version to 1.26.4

--- prisma.rb
+++ prisma.rb
@@ -2,8 +2,8 @@
 class Prisma < Formula
   desc "Prisma turns your database into a realtime GraphQL API"
   homepage "https://github.com/prisma/prisma"
-  url "https://teggnet.s3.amazonaws.com/prisma-1.26.4.tar.gz"
-  sha256 "[object"
+  url "https://teggnet.s3.ap-south-1.amazonaws.com/prisma-1.26.4.tar.gz"
+  sha256 "0fbd7190c51341de36f6242ca4d46db77db1e93796761731db244449523a514d"
   version "1.26.4"
 
   bottle :unneeded
